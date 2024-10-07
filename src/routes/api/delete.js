1b3389aa38a35e5df982af4503258453d6e64c92
swang308 <swang308@myseneca.ca>
2024-10-07 15:25:27 -0400

    add delete file

---
+++ src/routes/api/delete.js
@@ -0,0 +1,9 @@
+// src/routes/api/delete.js
+
+/**
+ * Handles deleting a fragment.
+ */
+module.exports = (req, res) => {
+  // Your logic to handle the DELETE request here.
+  res.status(200).json({ message: 'Fragment deleted successfully' });
+}
