517abe63ffebb649c8175bfcd90aea831b5624f0
swang308 <swang308@myseneca.ca>
2024-11-11 21:14:14 -0500

    add more tests

--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -2,11 +2,25 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 
-test('should return HTTP 404 response', async () => {
-  const res = await request(app).get('/404');
-  expect(res.statusCode).toBe(404);
+describe('app', () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test('testing 404 middleware', async () => {
+    const res = await request(app).get('/undefined');
+    expect(res.statusCode).toBe(404);
+  });
+
+  // Test for CORS headers
+  test('CORS headers are set', async () => {
+    const res = await request(app).get('/');
+    expect(res.headers['access-control-allow-origin']).toBe('*'); // Adjust if specific origin is set
+  });
+
+  // Test for gzip compression header, test for gzip compression larger
+  // test('gzip compression is enabled', async () => {
+  //   const res = await request(app).get('/');
+  //   expect(res.headers['content-encoding']).toBe('gzip');
+  // });
+
 });
-
-
 
 
