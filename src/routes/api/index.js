677cdcd525fb540c7ac8e479c8422bbf0d697bf6
swang308 <swang308@myseneca.ca>
2024-11-13 17:32:17 -0500

    add get route

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -14,18 +14,15 @@ const rawBody = () =>
   express.raw({
     inflate: true,
     limit: '5mb',
+
     type: (req) => {
       // See if we can parse this content type. If we can, `req.body` will be
       // a Buffer (e.g., `Buffer.isBuffer(req.body) === true`). If not, `req.body`
       // will be equal to an empty Object `{}` and `Buffer.isBuffer(req.body) === false`
       const { type } = contentType.parse(req);
-      if (!Fragment.isSupportedType(type)) {
-        throw new Error(`Unsupported Content-Type: ${type}`);
-      }
       return Fragment.isSupportedType(type);
     },
   });
-
 
 // POST /v1/fragments
 // Use a raw body parser for POST, which will give a `Buffer` Object or `{}` at `req.body`
@@ -35,8 +32,13 @@ router.post('/fragments', rawBody(), require('./post'));
 // GET /v1/fragments
 router.get('/fragments', require('./get'));
 // GET /v1/fragments/:id
-router.get('/fragments/:id', require('./getById'));
+router.get('/fragments/:id.:ext?', require('./getById'));
 router.get('/fragments/:id/info', require('./getByIdInfo'));
 
+// PUT /v1/fragments/:id
+// router.put('/fragments/:id', rawBody(), require('./put'));
+
+// DELETE /v1/fragments/:id
+// router.delete('/fragments/:id', require('./delete'));
 
 module.exports = router;
