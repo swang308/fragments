cb73a87d1697212b018a2897c117545ebba37ef3
swang308 <swang308@myseneca.ca>
2024-11-25 21:16:47 -0500

    accept S3

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,17 +1,19 @@
+// src/model/data/index.js
+
 const logger = require('../../logger');
 
 // This file will allow us to switch between different backend strategies (memory, AWS, etc.)
-let dataStrategy;
 
 // We can use an environment variable or other logic to determine which strategy to use.
 // For now, we're using the 'memory' strategy by default.
 if (process.env.DATA_BACKEND === 'aws') {
   logger.info('AWS backend is not implemented yet.');
 } else {
-  // Default to in-memory strategy
-  dataStrategy = require('./memory');
   logger.info('Using in-memory data strategy');
+  return require('./memory');
 }
 
-// Re-export the selected data strategy
-module.exports = dataStrategy;
+
+// If the environment sets an AWS Region, we'll use AWS backend
+// services (S3, DynamoDB); otherwise, we'll use an in-memory db.
+module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
