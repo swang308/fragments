6b3942979eb832b8ac74ea2b5589d8754ebc6832
swang308 <swang308@myseneca.ca>
2024-12-08 15:49:18 -0500

    change v1.0.0

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+// .eslintrc.js
+
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    // Add this next line to configure ESLint for Jest, see:
+    // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+    jest: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  rules: {},
+};
